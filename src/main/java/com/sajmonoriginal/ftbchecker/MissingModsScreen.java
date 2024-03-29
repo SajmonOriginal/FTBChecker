9a0f5845c21ba78dc5d1f39584b2be3e1943da1b
Jacob Sapoznikow <jacob1coder@gmail.com>
2024-03-28 18:02:46 -0700

    NeoForge 1.20 port!

--- src/main/java/com/sajmonoriginal/ftbchecker/MissingModsScreen.java
+++ src/main/java/com/sajmonoriginal/ftbchecker/MissingModsScreen.java
@@ -1,21 +1,26 @@
 package com.sajmonoriginal.ftbchecker;
 
-import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.Util;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
+import net.minecraftforge.fml.ModList;
 import org.jetbrains.annotations.NotNull;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MissingModsScreen extends Screen {
-    private final Component titleText = new TextComponent("You're missing mods");
-    private final Component subtitleText = new TextComponent("Click to download");
+    private final Component titleText = Component.literal("You're missing mods!");
+    private final Component subtitleText = Component.literal("Click to download:");
 
     public MissingModsScreen() {
-        super(new TextComponent("Missing Mods"));
+        super(Component.literal("Missing Mods"));
     }
 
     @Override
@@ -28,52 +33,111 @@ public class MissingModsScreen extends Screen {
         // Buttons for individual mods
         int modButtonY = topTextY + 45;
         int modButtonSpacing = 25;
-        addButtonForMod("FTBChunks", "https://www.curseforge.com/minecraft/mc-mods/ftb-chunks-forge/download/4494633", centerX - buttonWidth / 2, modButtonY);
-        addButtonForMod("FTBLibrary", "https://www.curseforge.com/minecraft/mc-mods/ftb-library-forge/download/4396792", centerX - buttonWidth / 2, modButtonY + modButtonSpacing);
-        addButtonForMod("FTBQuests", "https://legacy.curseforge.com/minecraft/mc-mods/ftb-quests-forge/download/4398375", centerX - buttonWidth / 2, modButtonY + modButtonSpacing * 2);
-        addButtonForMod("FTBTeams", "https://www.curseforge.com/minecraft/mc-mods/ftb-teams-forge/download/4375188", centerX - buttonWidth / 2, modButtonY + modButtonSpacing * 3);
+
+        int buttons = 0;
+
+        for (Map.Entry<String, String> entry : getMods().entrySet()) {
+            addButtonForMod(entry.getKey(), entry.getValue(), centerX - buttonWidth / 2, modButtonY + modButtonSpacing * buttons);
+            buttons++;
+        }
 
         // Button for downloading all mods
-        int downloadAllButtonY = modButtonY + modButtonSpacing * 4 + 10;
-        this.addRenderableWidget(new Button(centerX - buttonWidth / 2, downloadAllButtonY, buttonWidth, buttonHeight, new TextComponent("Download All"), button -> openDownloadPage()));
+        int downloadAllButtonY = modButtonY + modButtonSpacing * buttons + 10;
+        int gap = 8;
+
+        addRenderableWidget(
+                Button.builder(
+                        Component.literal("Download All"),
+                        button -> openDownloadPages()
+                ).bounds(
+                        centerX - buttonWidth / 2,
+                        downloadAllButtonY,
+                        buttonWidth / 2 - gap / 2,
+                        buttonHeight
+                ).build()
+        );
+
+        addRenderableWidget(
+                Button.builder(
+                        Component.literal("Ignore"),
+                        button -> exit()
+                ).bounds(
+                        centerX + gap / 2,
+                        downloadAllButtonY,
+                        buttonWidth / 2 - gap / 2,
+                        buttonHeight
+                ).build()
+        );
     }
 
     @Override
-    public void render(@NotNull PoseStack matrices, int mouseX, int mouseY, float delta) {
-        this.renderBackground(matrices);
-        super.render(matrices, mouseX, mouseY, delta);
+    public void render(@NotNull GuiGraphics graphics, int mouseX, int mouseY, float delta) {
+        renderBackground(graphics);
+
+        super.render(graphics, mouseX, mouseY, delta);
 
         // Render missing mods text
         int centerX = this.width / 2;
         int titleY = 10;
         int subtitleY = titleY + 15;
-        drawCenteredString(matrices, this.font, titleText, centerX, titleY, 16777215);
-        drawCenteredString(matrices, this.font, subtitleText, centerX, subtitleY, 16777215);
+
+        graphics.drawCenteredString(font, titleText, centerX, titleY, 16777215);
+        graphics.drawCenteredString(font, subtitleText, centerX, subtitleY, 16777215);
+    }
+
+    private Map<String, String> getMods() {
+        // This map has to be generated at runtime, otherwise the config won't work.
+        Map<String, String> mods = new HashMap<>();
+
+        if (FTBChecker.CONFIG.chunks && !ModList.get().isLoaded("ftbchunks"))
+            mods.put("FTB Chunks", "https://www.curseforge.com/minecraft/mc-mods/ftb-chunks-forge/download/" + FTBChecker.CONFIG.versions.chunksVersion);
+
+        if (FTBChecker.CONFIG.base && !ModList.get().isLoaded("ftblibrary"))
+            mods.put("FTB Library", "https://www.curseforge.com/minecraft/mc-mods/ftb-library-forge/download/" + FTBChecker.CONFIG.versions.baseVersion);
+
+        if (FTBChecker.CONFIG.quests && !ModList.get().isLoaded("ftbquests"))
+            mods.put("FTB Quests", "https://www.curseforge.com/minecraft/mc-mods/ftb-quests-forge/download/" + FTBChecker.CONFIG.versions.questsVersion);
+
+        if (FTBChecker.CONFIG.teams && !ModList.get().isLoaded("ftbteams"))
+            mods.put("FTB Teams", "https://www.curseforge.com/minecraft/mc-mods/ftb-teams-forge/download/" + FTBChecker.CONFIG.versions.teamsVersion);
+
+        if (FTBChecker.CONFIG.essentials && !ModList.get().isLoaded("ftbessentials"))
+            mods.put("FTB Essentials", "https://www.curseforge.com/minecraft/mc-mods/ftb-essentials-forge/download/" + FTBChecker.CONFIG.versions.essentialsVersion);
+
+        if (FTBChecker.CONFIG.ultimine && !ModList.get().isLoaded("ftbultimine"))
+            mods.put("FTB Ultimine", "https://www.curseforge.com/minecraft/mc-mods/ftb-ultimine-forge/download/" + FTBChecker.CONFIG.versions.ultimineVersion);
+
+        if (FTBChecker.CONFIG.backups && !ModList.get().isLoaded("ftbbackups2"))
+            mods.put("FTB Backups 2", "https://www.curseforge.com/minecraft/mc-mods/ftb-backups-2/download/" + FTBChecker.CONFIG.versions.backupsVersion);
+
+        return mods;
     }
 
     private void addButtonForMod(String modName, String downloadLink, int x, int y) {
         int buttonWidth = 200;
         int buttonHeight = 20;
-        this.addRenderableWidget(new Button(x, y, buttonWidth, buttonHeight, new TextComponent(modName), button -> openWebPage(downloadLink)));
+
+        addRenderableWidget(new Button.Builder(Component.literal(modName), button -> openWebPage(downloadLink)).bounds(x, y, buttonWidth, buttonHeight).build());
     }
 
+    private void exit() {
+        FTBChecker.IGNORED = true;
+
+        Minecraft.getInstance().setScreen(new TitleScreen());
+    }
+
+    @SuppressWarnings("CallToPrintStackTrace")
     private void openWebPage(String url) {
         try {
-            net.minecraft.Util.getPlatform().openUri(new URI(url));
+            Util.getPlatform().openUri(new URI(url));
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
     }
 
-    private void openDownloadPage() {
-        try {
-            openWebPage("https://www.curseforge.com/minecraft/mc-mods/ftb-chunks-forge/download/4494633");
-            openWebPage("https://www.curseforge.com/minecraft/mc-mods/ftb-library-forge/download/4396792");
-            openWebPage("https://legacy.curseforge.com/minecraft/mc-mods/ftb-quests-forge/download/4398375");
-            openWebPage("https://www.curseforge.com/minecraft/mc-mods/ftb-teams-forge/download/4375188");
-        } catch (Exception e) {
-            e.printStackTrace();
+    private void openDownloadPages() {
+        for (String url : getMods().values()) {
+            openWebPage(url);
         }
     }
-
 }
